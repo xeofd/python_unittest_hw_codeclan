90a11929675b01ed0ac15f1cbf8b0d8c2e4f8b8f
ben <benfd@outlook.com>
2020-07-21 19:43:42 +0100

    Added all tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -7,3 +7,12 @@ class TestCalculator(unittest.TestCase):
     # test functions
     def test_add(self):
         self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(2, subtract(5, 3))
+
+    def test_divide(self):
+        self.assertEqual(3, divide(21, 7))
+    
+    def test_multiply(self):
+        self.assertEqual(25, multiply(5, 5))
